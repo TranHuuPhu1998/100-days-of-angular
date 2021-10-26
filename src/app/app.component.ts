7ce1af109d060dde48cb6c6aab5edef0eba41abd
Tran Huu Phu <phu.tran@digitalunicorn.fr>
2021-10-26 15:19:37 +0700

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren, ViewContainerRef } from '@angular/core';
+import { ToggleComponent } from './toggle/toggle.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dataBinding';
+  name  = 'phu tran';
+  checked = true;
+
+  @ViewChild('toggleComp') toggleComp: ToggleComponent = new ToggleComponent;
+  @ViewChild('nameForm', {
+    read: ElementRef,
+    static: true
+  })
+  nameForm: ElementRef<HTMLFormElement> | undefined;
+
+  @ViewChildren(ToggleComponent) toggleList: QueryList<ToggleComponent> | undefined;
+
+  ngOnInit(){
+    setTimeout(() => {
+      this.nameForm?.nativeElement.focus();
+    },2000);
+  }
+
+  ngAfterViewInit() {
+    console.log(this.toggleList?.changes.subscribe());
+  }
+
+  toggleInside() {
+    this.toggleComp.toggle();
+  }
 }
